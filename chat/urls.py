acb7d8c006f725036c015346120a3e0c9389e235
David Paul <dfp333@gmail.com>
2018-09-07 16:09:30 -0500

    fixed login

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+    url(r'^helpdesk$', views.room, name='room'),
     url(r'^register$', views.register, name='register'),
     url(r'^login$', views.login_user, name='login'),
     url(r'^logout$', views.user_logout, name='logout'),
